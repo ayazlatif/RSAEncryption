ed530c5999cb6787c39942954f0111a111ee3816
alatif <alatif@uw.edu>
2018-12-03 00:44:06 -0800

    fixed phi and added euclid alg

--- js/rsa.js
+++ js/rsa.js
@@ -1,30 +1,87 @@
 
 function decrypt(e, n) {
    function phi(n) {
+
+      // compute prime factorization of n
       var search = n;
       var factors = [];
+      var count = 0;
       while (n % 2 == 0) {
-         factors.push(2);
+         count++;
          n /= 2;
       }
+      if (count > 0) {
+         factors.push([2, count]);
+      }
+      // storing [p1^e1,p2^e2...]
       for (var i = 3; i <= Math.sqrt(search); i += 2) {
+         count = 0;
          while (n % i == 0) {
-            console.log(i);
-            factors.push(i);
+            count++;
             n /= i;
+         }
+         if (count > 0) {
+            factors.push([i, count]);
          }
       }
       if (n > 2) {
-         factors.push(n);
+         factors.push([n, 1]);
       }
 
+      // phi(n) = phi(p1)phi(p2)...phi(pn)
+      // phi(pi) = (pi^ei - pi^(ei - 1))
       var result = 1;
       for (var i = 0; i < factors.length; i++) {
-         result *= (factors[i] - 1);
+         var num = factors[i];
+         console.log(num);
+         result *= (Math.pow(num[0], num[1]) - Math.pow(num[0], num[1] - 1));
       }
       return result;
    }
+
+   // return format is [b = rx + sy] ==> [b, x, y]  
+   function extendedEuclid(a, b) {
+      a = +a;
+      b = +b;
+      if (a !== a || b !== b) {
+         return [NaN, NaN, NaN];
+      }
+
+      if (a === Infinity || a === -Infinity || b === Infinity || b === -Infinity) {
+         return [Infinity, Infinity, Infinity];
+      }
+      // Checks if a or b are decimals
+      if ((a % 1 !== 0) || (b % 1 !== 0)) {
+         return false;
+      }
+      var signX = (a < 0) ? -1 : 1,
+         signY = (b < 0) ? -1 : 1,
+         x = 0,
+         y = 1,
+         u = 1,
+         v = 0,
+         q, r, m, n;
+      a = Math.abs(a);
+      b = Math.abs(b);
+
+      while (a !== 0) {
+         q = Math.floor(b / a);
+         r = b % a;
+         m = x - u * q;
+         n = y - v * q;
+         b = a;
+         a = r;
+         x = u;
+         y = v;
+         u = m;
+         v = n;
+      }
+      return [b, signX * x, signY * y];
+   }
+
    var phiOfN = phi(2747);
-   alert(phiOfN);
+   console.log("phi(n) = " + phiOfN)
+   var d = extendedEuclid(13, phiOfN)[1];
+   // TODO handle case when d is negative
 
 }
